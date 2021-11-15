5b1480da47cccd55afa48249c24eb90b3642e667
Jakub Klemsa <jakub.klemsa@fel.cvut.cz>
2021-11-15 15:17:40 +0100

    112-bit parameter sets.

--- src/params.rs
+++ src/params.rs
@@ -60,7 +60,213 @@ pub const PARMXX__TRIVIAL: Params = Params {
        ks_level: 3,
 };
 
-/// TFHE Parameter Set A
+
+// =============================================================================
+//
+//  112-bit security
+//
+
+/// TFHE Parameter Set A (112.4-bit security)
+#[allow(dead_code)]
+pub const PARM112__PI_2__D_02__A: Params = Params {
+           maxlen: 32,
+    bit_precision: 2,
+      quad_weight: 2,
+       lwe_params: LWEParams {
+        dimension: 500,
+     log2_std_dev: -13, // -13.31,
+    },
+    rlwe_params: RLWEParams {
+        polynomial_size: 1024,
+              dimension: 1,
+           log2_std_dev: -24, // -24.86,
+    },
+    bs_base_log: 8,
+       bs_level: 2,
+    ks_base_log: 1,
+       ks_level: 11,
+};
+
+/// TFHE Parameter Set B (112.5-bit security)
+#[allow(dead_code)]
+pub const PARM112__PI_2__D_03__B: Params = Params {
+           maxlen: 32,
+    bit_precision: 2,
+      quad_weight: 3,
+       lwe_params: LWEParams {
+        dimension: 510,
+     log2_std_dev: -13, // -13.61,
+    },
+    rlwe_params: RLWEParams {
+        polynomial_size: 1024,
+              dimension: 1,
+           log2_std_dev: -25, // -25.17,
+    },
+    bs_base_log: 8,
+       bs_level: 2,
+    ks_base_log: 1,
+       ks_level: 11,
+};
+
+/// TFHE Parameter Set C (111.5-bit security)
+#[allow(dead_code)]
+pub const PARM112__PI_3__D_19__C: Params = Params {
+           maxlen: 32,
+    bit_precision: 3,
+      quad_weight: 19,
+       lwe_params: LWEParams {
+        dimension: 590,
+     log2_std_dev: -16, // -16.11,
+    },
+    rlwe_params: RLWEParams {
+        polynomial_size: 1024,
+              dimension: 1,
+           log2_std_dev: -28, // -28.60,
+    },
+    bs_base_log: 9,
+       bs_level: 2,
+    ks_base_log: 1,
+       ks_level: 14,
+};
+
+
+// -----------------------------------------------------------------------------
+
+/// TFHE Parameter Set D (112.1-bit security)
+#[allow(dead_code)]
+pub const PARM112__PI_3__D_12__D: Params = Params {
+           maxlen: 32,
+    bit_precision: 3,
+      quad_weight: 12,
+       lwe_params: LWEParams {
+        dimension: 580,
+     log2_std_dev: -15, // -15.73,
+    },
+    rlwe_params: RLWEParams {
+        polynomial_size: 1024,
+              dimension: 1,
+           log2_std_dev: -28, // -28.26,
+    },
+    bs_base_log: 9,
+       bs_level: 2,
+    ks_base_log: 1,
+       ks_level: 13,
+};
+
+/// TFHE Parameter Set E (112.8-bit security)
+#[allow(dead_code)]
+pub const PARM112__PI_4__D_12__E: Params = Params {
+           maxlen: 32,
+    bit_precision: 4,
+      quad_weight: 12,
+       lwe_params: LWEParams {
+        dimension: 620,
+     log2_std_dev: -16, // -16.78,
+    },
+    rlwe_params: RLWEParams {
+        polynomial_size: 1024,
+              dimension: 1,
+           log2_std_dev: -27, // -27.60,
+    },
+    bs_base_log: 7,
+       bs_level: 3,
+    ks_base_log: 1,
+       ks_level: 14,
+};
+
+/// TFHE Parameter Set F (111.5-bit security)
+#[allow(dead_code)]
+pub const PARM112__PI_5__D_20__F: Params = Params {
+           maxlen: 32,
+    bit_precision: 5,
+      quad_weight: 20,
+       lwe_params: LWEParams {
+        dimension: 680,
+     log2_std_dev: -18, // -18.25,
+    },
+    rlwe_params: RLWEParams {
+        polynomial_size: 1024,
+              dimension: 1,
+           log2_std_dev: -29, // -29.04,
+    },
+    bs_base_log: 7,
+       bs_level: 3,
+    ks_base_log: 1,
+       ks_level: 16,
+};
+
+// -----------------------------------------------------------------------------
+
+/// TFHE Parameter Set G (113.1-bit security)
+#[allow(dead_code)]
+pub const PARM112__PI_4__D_36__G: Params = Params {
+           maxlen: 32,
+    bit_precision: 4,
+      quad_weight: 36,
+       lwe_params: LWEParams {
+        dimension: 650,
+     log2_std_dev: -17, // -17.62,
+    },
+    rlwe_params: RLWEParams {
+        polynomial_size: 1024,
+              dimension: 1,
+           log2_std_dev: -28, // -28.42,
+    },
+    bs_base_log: 7,
+       bs_level: 3,
+    ks_base_log: 1,
+       ks_level: 15,
+};
+
+/// TFHE Parameter Set H (112.1-bit security)
+#[allow(dead_code)]
+pub const PARM112__PI_5__D_36__H: Params = Params {
+           maxlen: 32,
+    bit_precision: 5,
+      quad_weight: 36,
+       lwe_params: LWEParams {
+        dimension: 680,
+     log2_std_dev: -18, // -18.67,
+    },
+    rlwe_params: RLWEParams {
+        polynomial_size: 1024,
+              dimension: 1,
+           log2_std_dev: -28, // -28.67,
+    },
+    bs_base_log: 6,
+       bs_level: 4,
+    ks_base_log: 1,
+       ks_level: 16,
+};
+
+/// TFHE Parameter Set I (112.0-bit security)
+#[allow(dead_code)]
+pub const PARM112__PI_7__D_74__I: Params = Params {
+           maxlen: 32,
+    bit_precision: 7,
+      quad_weight: 74,
+       lwe_params: LWEParams {
+        dimension: 820,
+     log2_std_dev: -22, // -22.85,
+    },
+    rlwe_params: RLWEParams {
+        polynomial_size: 8192,
+              dimension: 1,
+           log2_std_dev: -50, // -50.82,
+    },
+    bs_base_log: 25,
+       bs_level: 1,
+    ks_base_log: 1,
+       ks_level: 20,
+};
+
+
+// =============================================================================
+//
+//  90-bit security
+//
+
+/// TFHE Parameter Set A (90-bit security)
 #[allow(dead_code)]
 pub const PARM90__PI_2__D_02__A: Params = Params {
            maxlen: 32,
@@ -81,7 +287,7 @@ pub const PARM90__PI_2__D_02__A: Params = Params {
        ks_level: 11,
 };
 
-/// TFHE Parameter Set B
+/// TFHE Parameter Set B (90-bit security)
 #[allow(dead_code)]
 pub const PARM90__PI_2__D_03__B: Params = Params {
            maxlen: 32,
@@ -102,7 +308,7 @@ pub const PARM90__PI_2__D_03__B: Params = Params {
        ks_level: 11,
 };
 
-/// TFHE Parameter Set C
+/// TFHE Parameter Set C (90-bit security)
 #[allow(dead_code)]
 pub const PARM90__PI_3__D_19__C: Params = Params {
            maxlen: 32,
@@ -125,7 +331,7 @@ pub const PARM90__PI_3__D_19__C: Params = Params {
 
 // -----------------------------------------------------------------------------
 
-/// TFHE Parameter Set D
+/// TFHE Parameter Set D (90-bit security)
 #[allow(dead_code)]
 pub const PARM90__PI_3__D_12__D: Params = Params {
            maxlen: 32,
@@ -146,7 +352,7 @@ pub const PARM90__PI_3__D_12__D: Params = Params {
        ks_level: 13,
 };
 
-/// TFHE Parameter Set E
+/// TFHE Parameter Set E (90-bit security)
 #[allow(dead_code)]
 pub const PARM90__PI_4__D_12__E: Params = Params {
            maxlen: 32,
@@ -167,7 +373,7 @@ pub const PARM90__PI_4__D_12__E: Params = Params {
        ks_level: 14,
 };
 
-/// TFHE Parameter Set F
+/// TFHE Parameter Set F (90-bit security)
 #[allow(dead_code)]
 pub const PARM90__PI_5__D_20__F: Params = Params {
            maxlen: 32,
@@ -190,7 +396,7 @@ pub const PARM90__PI_5__D_20__F: Params = Params {
 
 // -----------------------------------------------------------------------------
 
-/// TFHE Parameter Set G
+/// TFHE Parameter Set G (90-bit security)
 #[allow(dead_code)]
 pub const PARM90__PI_4__D_36__G: Params = Params {
            maxlen: 32,
@@ -211,7 +417,7 @@ pub const PARM90__PI_4__D_36__G: Params = Params {
        ks_level: 15,
 };
 
-/// TFHE Parameter Set H
+/// TFHE Parameter Set H (90-bit security)
 #[allow(dead_code)]
 pub const PARM90__PI_5__D_36__H: Params = Params {
            maxlen: 32,
@@ -232,7 +438,7 @@ pub const PARM90__PI_5__D_36__H: Params = Params {
        ks_level: 16,
 };
 
-/// TFHE Parameter Set I
+/// TFHE Parameter Set I (90-bit security)
 #[allow(dead_code)]
 pub const PARM90__PI_7__D_74__I: Params = Params {
            maxlen: 32,
