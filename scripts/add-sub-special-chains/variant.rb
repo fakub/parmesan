4210cd2f1a2b5053e2b9496943099a86573083cd
Jakub Klemsa <jakub.klemsa@fel.cvut.cz>
2022-06-02 17:48:40 +0200

    Add-Sub chains: resources-saving approach (very different).

--- scripts/add-sub-special-chains/variant.rb
+++ scripts/add-sub-special-chains/variant.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
-LIMIT_LOG = 11
+LIMIT_WID = 11
+#~ DBG = true
 DBG = false
 
 # equivalence class containing the only positive odd number as its representative
@@ -42,8 +43,12 @@ class OddClass
         end
     end
 
+    def unit?
+        @p.nil?
+    end
+
     def to_s
-        "( #{@val}" + (@p.nil? ? "" : " | #{@p_pos ? ' ' : '-'}#{@p.val} #{@q_pos ? '+' : '-'} #{@q.val}·2^#{@r}") + " )"
+        "◖ #{@val}" + (@p.nil? ? "" : " = #{@p_pos ? ' ' : '-'}#{@p.val} #{@q_pos ? '+' : '-'} #{@q.val}·2^#{@r}") + " ◗"
     end
     alias inspect to_s
 
@@ -56,29 +61,67 @@ class OddClass
     #~ end
 end
 
-def extend_chain(chain, db)
-    raise "[extend_chain] Invalid chain contents." unless chain.map{|e| e.class == OddClass }.reduce(:&)
+class ASChain < Array
+
+    #~ def initialize(*args)
+        #~ super *args
+        #~ raise "[ASChain::new] Invalid chain contents (expecting OddClass)." unless chain.map{|e| e.class == OddClass }.reduce(:&)
+        #~ raise "[ASChain::new] Invalid chain contents (expecting OddClass)." unless chain
+    #~ end
+
+    def merge(other)
+        raise "[ASChain::#{__method__}] Merging with non-ASChain class." unless other.class == ASChain
+        difi = self.size
+        self.zip(other).each.with_index do |p,i|
+            if p[0].nil? or p[1].nil? or p[0].val != p[1].val
+                difi = i
+                break
+            end
+        end
+        raise "[ASChain::#{__method__}] Chains differ even in first element, which is supposed to be ◖ 1 ◗." if difi == 0
+        ASChain[*(self + other[difi..])]
+    end
+
+    def to_s
+        #~ "C:" + super
+        super
+    end
+    alias inspect to_s
+
+end
+
+def extend_chains_ary(chains_arys, db)
+    # check proper structure of chains array
+    # expects [[ASChain[1]], [ASChain[1,3],ASChain[1,5],...]]
+    raise "[#{__method__}] Invalid structure of array of arrays of chains (or wrong chains' length)." \
+        unless chains_arys.map.with_index{|cary,ci| cary.map{|c| c.class == ASChain and c.size == ci+1 }.reduce(:&)}.reduce(:&)
 
     ecs = []
+    lvl = chains_arys.last.last.size
 
     puts "Loop p ..." if DBG
-    # loop p
-    chain.each do |p|
-        puts "  p = #{p}" if DBG
+    # loop p (in all existing chains)
+    chains_arys.flatten(1).each do |cp|
+        puts "  chain for p = #{cp}" if DBG
         # loop q
-        chain.each do |q|
-            puts "    q = #{q}" if DBG
-            #~ (1..LIMIT_LOG).each do |r|
-            (1..LIMIT_LOG-q.wid).each do |r|
+        chains_arys.flatten(1).each do |cq|
+            puts "    chain for q = #{cq}" if DBG
+
+            # check if the combination of chosen chains gives expected length
+            cpq = cp.merge(cq)
+            next if cpq.size != lvl
+
+            #~ (1..LIMIT_WID).each do |r|
+            (1..LIMIT_WID-cq.last.wid).each do |r|
                 puts "      r = #{r}" if DBG
                 [true, false].each do |p_pos|
                     [true, false].each do |q_pos|
                         begin
-                            on = OddClass.new(p_pos, p, q_pos, q, r)
-                            if on.val > 0
+                            on = OddClass.new(p_pos, cp.last, q_pos, cq.last, r)
+                            if on.val > 0 and db[on.val].nil?
                                 puts "      Adding #{on}" if DBG
-                                ecs << (chain + [on])
-                                db[on.val] = chain + [on] if db[on.val].nil?
+                                ecs << ASChain[*(cpq + [on])]
+                                db[on.val] = ASChain[*(cpq + [on])]
                             end
                         rescue => error
                             puts "Skipping: #{error}" if DBG
@@ -92,24 +135,27 @@ def extend_chain(chain, db)
     ecs
 end
 
-# init chain
-chain = [OddClass.new]
-db = {1 => [OddClass.new]}
+# init chains array with [[◖ 1 ◗]]
+chains = [[ASChain[OddClass.new]]]
+# init DB of values <=> chains
+db = {1 => ASChain[OddClass.new]}
 
 # 1st extend
-ecs = extend_chain chain, db
+chains << extend_chains_ary(chains, db)
 
 # 2nd extend
-eccs = []
-ecs.each do |ec|
-    eccs += extend_chain ec, db
-end
+chains << extend_chains_ary(chains, db)
 
 # 3rd extend
-ecccs = []
-eccs.each do |ecc|
-    ecccs += extend_chain ecc, db
-end
+chains << extend_chains_ary(chains, db)
+
+# 4th extend
+chains << extend_chains_ary(chains, db)
+
+#~ ecccs = []
+#~ eccs.each do |ecc|
+    #~ ecccs += extend_chain ecc, db
+#~ end
 
 #~ # 4th extend
 #~ # eccccs = []
@@ -126,7 +172,7 @@ db.sort_by{|v,c| v }.each do |v, c|
         puts "----"
         i = v
     end
-    if v > (1 << (LIMIT_LOG-1)) and not wpr
+    if v > (1 << (LIMIT_WID-1)) and not wpr
         puts "====    no guarantee    ===="
         wpr = true
     end
