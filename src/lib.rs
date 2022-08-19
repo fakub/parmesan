9ed97fe9d196f9fdabb06d898f19aebb6c5b92c0
Jakub Klemsa <jakub.klemsa@fel.cvut.cz>
2022-08-19 10:19:56 +0200

    Minor: TODOs.

--- src/lib.rs
+++ src/lib.rs
@@ -178,6 +178,7 @@ pub static ref ASC_12: BTreeMap<usize, Vec<AddShift>> = Asc::map_from_yaml(ASC_B
 //
 //  TODO / Wishlist
 //
+//  - check optimality of squaring (incorrect estimates for longer inputs)
 //  - merge with TODOs file
 //  - mul_lwe .. for more than 1x1 bit? let say 1x2 bit? what about squaring?
 //  - parallel mulary reduction
