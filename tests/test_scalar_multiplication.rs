519f8f1394d3012948c445c99ba007eec4231dae
Jakub Klemsa <jakub.klemsa@fel.cvut.cz>
2023-02-22 21:01:39 +0100

    Tests: special cases in scalar mul.

--- tests/test_scalar_multiplication.rs
+++ tests/test_scalar_multiplication.rs
@@ -24,6 +24,40 @@ fn t_scm_non_triv() {
 }
 
 #[test]
+/// Scalar multiplication of trivial sub-samples only.
+fn t_scm_all_triv() {
+    println!("All-Triv ...");
+    t_impl_scm_with_mode(EncrVsTriv::TRIV);
+}
+
+#[test]
+/// Scalar multiplication of mixed sub-samples.
+fn t_scm_some_triv() {
+    println!("Mixed ...");
+    t_impl_scm_with_mode(EncrVsTriv::ENCRTRIV);
+}
+
+// Special
+
+#[test]
+/// Scalar multiplication by specific values
+fn t_scm_pow_2() {
+    for mode in [EncrVsTriv::ENCR, EncrVsTriv::TRIV, EncrVsTriv::ENCRTRIV] {
+        t_impl_scm_with_mode_and_scalar(mode, 0);
+        t_impl_scm_with_mode_and_scalar(mode, 1);
+        t_impl_scm_with_mode_and_scalar(mode,-1);
+        t_impl_scm_with_mode_and_scalar(mode, 2);
+        t_impl_scm_with_mode_and_scalar(mode,-2);
+        t_impl_scm_with_mode_and_scalar(mode, 4);
+        t_impl_scm_with_mode_and_scalar(mode,-4);
+        t_impl_scm_with_mode_and_scalar(mode, 256);
+        t_impl_scm_with_mode_and_scalar(mode,-256);
+    }
+}
+
+// ASC*, Koyama-Tsuruoka
+
+#[test]
 /// Addition-Subtraction Chains: non-emptiness & correctness.
 fn t_asc() {
     println!("ASC ...");
@@ -63,21 +97,6 @@ fn t_koy_tsu() {
     }
 }
 
-//WISH
-//~ #[test]
-//~ /// Scalar multiplication of trivial sub-samples only.
-//~ fn t_scm_all_triv() {
-    //~ println!("All-Triv ...");
-    //~ t_impl_scm_with_mode(EncrVsTriv::TRIV);
-//~ }
-
-//~ #[test]
-//~ /// Scalar multiplication of mixed sub-samples.
-//~ fn t_scm_some_triv() {
-    //~ println!("Mixed ...");
-    //~ t_impl_scm_with_mode(EncrVsTriv::ENCRTRIV);
-//~ }
-
 
 // -----------------------------------------------------------------------------
 //  Test Implementations
@@ -88,38 +107,35 @@ fn t_impl_scm_with_mode(mode: EncrVsTriv) {
     let mut rng = rand::thread_rng();
 
     for _ in 0..common::TESTS_REPEAT_SCM {
-        // generate random vector(s)
-        let m1_vec = gen_rand_vec(common::TESTS_BITLEN_SCM);
-        // convert to integer(s)
-        let m1 = encryption::convert_from_vec(&m1_vec).expect("convert failed.");
         // generate random scalar
         let k: i32 = rng.gen_range(-(1 << common::TESTS_BITLEN_SCALAR)..=(1 << common::TESTS_BITLEN_SCALAR));
-
-        println!("  m = {} ({}-bit: {:?})\n  k = {}", m1, common::TESTS_BITLEN_SCM, m1_vec, k);
-
-        // encrypt -> homomorphic eval -> decrypt
-        let c1 = encrypt_with_mode(&m1_vec, mode);
-        let c_he = ParmArithmetics::scalar_mul(&common::TEST_PC, k, &c1);
-        let m_he = common::TEST_PU.decrypt(&c_he).expect("ParmesanUserovo::decrypt failed.");
-
-        // plain eval
-        let m_pl = ParmArithmetics::scalar_mul(&common::TEST_PC, k, &m1);
-
-        println!("  scm = {} (exp. {})", m_he, m_pl);
-
-        // compare results
-        assert_eq!(m_he, m_pl);
+        // run test with mode & scalar
+        t_impl_scm_with_mode_and_scalar(mode, k);
     }
 }
 
+/// Implementation for fixed mode & scalar
+fn t_impl_scm_with_mode_and_scalar(
+    mode: EncrVsTriv,
+    k: i32,
+) {
+    // generate random vector(s)
+    let m1_vec = gen_rand_vec(common::TESTS_BITLEN_SCM);
+    // convert to integer(s)
+    let m1 = encryption::convert_from_vec(&m1_vec).expect("convert failed.");
 
+    println!("  m = {} ({}-bit: {:?})\n  k = {}", m1, common::TESTS_BITLEN_SCM, m1_vec, k);
 
+    // encrypt -> homomorphic eval -> decrypt
+    let c1 = encrypt_with_mode(&m1_vec, mode);
+    let c_he = ParmArithmetics::scalar_mul(&common::TEST_PC, k, &c1);
+    let m_he = common::TEST_PU.decrypt(&c_he).expect("ParmesanUserovo::decrypt failed.");
 
+    // plain eval
+    let m_pl = ParmArithmetics::scalar_mul(&common::TEST_PC, k, &m1);
 
-// #############################################################################
+    println!("  scm = {} (exp. {})", m_he, m_pl);
 
-
-
-//TODO
-//  k = 0, 1, -1
-//  k = 2^n
+    // compare results
+    assert_eq!(m_he, m_pl);
+}
